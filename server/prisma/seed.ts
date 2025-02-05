77a9a715a4b339e7b9f4bece62033e44cebb952b
albqvictor1508 <albq.victor@gmail.com>
2025-02-05 15:08:01 -0300

    chore: add seed and change on project files

---
+++ server/prisma/seed.ts
@@ -0,0 +1,28 @@
+import { db } from "./db";
+import { faker } from "@faker-js/faker";
+
+const user1 = await db.users.create({
+	data: {
+		email: faker.internet.email(),
+		password: faker.internet.password(),
+		birthDate: faker.date.birthdate(),
+	},
+});
+
+const user2 = await db.users.create({
+	data: {
+		email: faker.internet.email(),
+		password: faker.internet.password(),
+		birthDate: faker.date.birthdate(),
+	},
+});
+
+const user3 = await db.users.create({
+	data: {
+		email: faker.internet.email(),
+		password: faker.internet.password(),
+		birthDate: faker.date.birthdate(),
+	},
+});
+
+console.log({ user1, user2, user3 });
--- server/src/routes/auth/new.ts
+++ server/src/routes/auth/new.ts
@@ -1,16 +1,22 @@
 import { t } from "elysia";
 import type { app } from "../../server";
+import { db } from "../../../prisma/db";
 
 export const route = (elysia: typeof app) => {
 	elysia.post(
 		"/auth/new",
-		async () => {
-			return "ta pegando aq";
+		async ({ body }) => {
+			const { email, password, birthDate } = body;
+
+			const user = await db.users.create({
+				data: { email, password, birthDate },
+			});
 		},
 		{
 			body: t.Object({
 				email: t.String({ format: "email" }),
 				password: t.String(),
+				birthDate: t.Date(),
 			}),
 		},
 	);
