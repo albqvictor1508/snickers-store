3c2d6c2cebe28bb5d3afebb5a80f4ce8e4c1aade
albqvictor1508 <albq.victor@gmail.com>
2025-02-05 12:43:01 -0300

    add postgre on project's docker compose

--- server/src/routes/testee.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "../server";
-
-export const route = async (elysia: typeof app) => {
-    elysia.get("/test", async () => {
-        return {message: "bom dia porra"}
-    })
-} 
