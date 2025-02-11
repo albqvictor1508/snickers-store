08fe73fad24a9085d023227a790e15b826bb5602
albqvictor1508 <albq.victor@gmail.com>
2025-02-11 14:09:34 -0300

    changes on swagger, part 1

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,11 @@ import { loadRoutes } from "./utils/load-routes";
 import swagger from "@elysiajs/swagger";
 
 export const app = new Elysia().use(
-	swagger({ path: "/docs", autoDarkMode: true }),
+	swagger({
+		path: "/docs",
+		documentation: { info: { title: "Snickers Store", version: "0.0.1" } },
+		autoDarkMode: true,
+	}),
 );
 
 await loadRoutes();
